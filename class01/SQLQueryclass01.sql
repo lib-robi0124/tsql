66a05e37debd3f7bbc524ce9a74980f2b7ff8e2d
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-04-03 08:50:59 +0200

    dcfv

--- class01/SQLQueryclass01.sql
+++ class01/SQLQueryclass01.sql
@@ -1,13 +1,13 @@
 USE [master] 
 GO
--- DROP DATABASE IF EXISTS [SEDC_NEW]
+DROP DATABASE IF EXISTS [SEDC_NEW]
 IF EXISTS (SELECT name FROM sys.databases WHERE name = N'SEDC')
 	ALTER DATABASE [SEDC] SET  SINGLE_USER WITH ROLLBACK IMMEDIATE
 GO
 
-CREATE DATABASE [SEDC_NEW]
+CREATE DATABASE [SEDC]
 GO
-USE [SEDC_NEW]
+USE [SEDC]
 GO
 
 -- CREATE TABLE
@@ -71,3 +71,78 @@ WHERE Id = 3
 
 -- DROP COMMAND - Deletes whole table 
 DROP TABLE Customers
+
+DROP TABLE IF EXISTS [dbo].[OrderDetails]
+DROP TABLE IF EXISTS [dbo].[Orders]
+DROP TABLE IF EXISTS [dbo].[BusinessEntities]
+DROP TABLE IF EXISTS [dbo].[Customers]
+DROP TABLE IF EXISTS [dbo].[Employees]
+DROP TABLE IF EXISTS [dbo].[Products]
+GO
+
+CREATE TABLE Customers (
+	[Id] int IDENTITY(1,1) NOT NULL,
+	[Name] nvarchar(100) NOT NULL,
+	[AccountNumber] nvarchar(100) NULL,
+	[City] nvarchar(100) NULL,
+	[RegionName] nvarchar(100) NULL,
+	[CustomerSize] nvarchar(10) NULL,
+	[PhoneNumber] nvarchar(20) NULL,
+	[IsActive] bit NOT NULL,
+CONSTRAINT [PK_Customers] PRIMARY KEY (Id)
+)
+GO
+
+CREATE TABLE Employees (
+	[Id] int IDENTITY(1,1) NOT NULL,
+	[FirstName] nvarchar(100) NOT NULL,
+	[LastName] nvarchar(100) NOT NULL,
+	[DateOfBirth] date NULL, 
+	[Gender] nchar(1) NULL, -- 'F' / 'M'
+	[HireDate] date NULL,
+	[NationalIdNumber] nvarchar(20) NULL
+CONSTRAINT [PK_Employees] PRIMARY KEY (Id)
+)
+GO
+CREATE TABLE Products (
+	[Id] int IDENTITY(1,1) NOT NULL,
+	[Code] nvarchar(50) NULL,
+	[Name] nvarchar(100) NULL,
+	[Description] nvarchar(max) NULL,
+	[Weight] decimal(18, 2) NULL,
+	[Price] decimal(18,2) NULL,
+	[Cost] decimal(18,2) NULL,
+CONSTRAINT [PK_Products] PRIMARY KEY (Id)
+)
+GO
+CREATE TABLE Orders(
+	[Id] bigint IDENTITY(1,1) NOT NULL,
+	[OrderDate] date NULL,
+	[Status] smallint NULL,
+	[BusinessEntityId] int NULL,
+	[CustomerId] int NULL,
+	[EmployeeId] int NULL,
+	[TotalPrice] decimal(18,2) NULL,
+	[Comment] nvarchar(max) NULL
+CONSTRAINT [PK_Orders] PRIMARY KEY (Id)
+)
+GO
+
+CREATE TABLE OrderDetails(
+	[Id] int IDENTITY(1,1) NOT NULL,
+	[OrderId] bigint NULL,
+	[ProductId] int NULL,
+	[Quantity] int NULL,
+	[Price] decimal(18,2) NULL,
+CONSTRAINT [PK_OrderDetails] PRIMARY KEY (Id)
+)
+GO
+CREATE TABLE BusinessEntities(
+	[Id] int IDENTITY(1,1) NOT NULL,
+	[Name] nvarchar(100) NULL,
+	[Region] nvarchar(1000) NULL,
+	[Zipcode] nvarchar(10) NULL,
+	[Size] nvarchar(10) NULL,
+CONSTRAINT [PK_BusinessEntities] PRIMARY KEY (Id)
+)
+GO
