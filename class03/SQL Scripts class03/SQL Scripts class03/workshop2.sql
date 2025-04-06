ae14c723b0303e3daef4982b2b398194406a746d
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-04-06 14:18:01 +0200

    Create workshop2.sql

---
+++ class03/SQL Scripts class03/SQL Scripts class03/workshop2.sql
@@ -0,0 +1,29 @@
+/*
+Calculate the total amount per BusinessEntity on all orders in the
+system and filter only total orders greater than 635558 */
+
+SELECT be.Name, SUM(o.TotalPrice) AS TotalAmount
+FROM Orders o
+INNER JOIN BusinessEntities be
+ON o.BusinessEntityId = be.Id
+GROUP BY be.Name
+HAVING SUM(o.TotalPrice) > 635558;
+
+/*
+Calculate the total amount per BusinessEntity on all orders in the
+system from Customers with ID < 20 and filter only total orders
+less than 100000 */
+
+SELECT be.Name AS BusinessEntityName, c.Name AS CustomerName, SUM(o.TotalPrice) AS TotalAmount
+FROM Orders o
+INNER JOIN BusinessEntities be ON o.BusinessEntityId = be.Id
+INNER JOIN Customers c ON o.CustomerId = c.Id WHERE c.Id < 20
+GROUP BY be.Name, c.Name
+HAVING SUM(o.TotalPrice) < 100000;
+
+/*
+Find the Maximal Order amount, and the Average Order amount
+per BusinessEntity on all orders in the system. Filter only records
+where Total order amount is more than 4x bigger than average */
+
+
