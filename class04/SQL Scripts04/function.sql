f8c594a7e3af66a3b2c82de793c90d5316fb4e1f
lib-robi0124 <robert.ristovski@libertysteelgroup.com>
2025-04-10 17:33:58 +0200

    g

--- class04/SQL Scripts04/function.sql
+++ class04/SQL Scripts04/function.sql
@@ -13,3 +13,28 @@ BEGIN
 END
 
 select dbo.fn_EmployeeFullName(40)
+
+SELECT * FROM Employees
+
+CREATE FUNCTION fn_GetEmpoyeesByGender(@Gender NCHAR)
+RETURNS @EmployeesByGender TABLE (FirstName VARCHAR(50), LastName VARCHAR(50), Gender NCHAR)
+AS
+	BEGIN
+		IF @Gender = 'M'
+		begin
+			insert into @EmployeesByGender
+			select FirstName, LastName, Gender
+			from Employees
+			where Gender = 'M'
+		end
+		else
+		begin
+			insert into @EmployeesByGender
+			select FirstName, LastName, Gender
+			from Employees
+			where Gender = 'F'
+		end
+		return 
+	END
+
+	select * from dbo.fn_EmployesByGender('M') 
